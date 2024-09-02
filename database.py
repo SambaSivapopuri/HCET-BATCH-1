29dcfee7d41728536790a08aebb7d7e9c4e0cce7
Sambasivarao <163990845+SambaSivapopuri@users.noreply.github.com>
2024-09-02 16:22:22 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,8 @@
+db_config = {
+    'user': 'your_mysql_user',
+    'password': 'your_mysql_password',
+    'host': 'localhost',
+    'database': 'your_database_name',
+}
+
+JWT_SECRET_KEY="HCET-BATCH-CSE-2024"
